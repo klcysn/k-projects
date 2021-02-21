6602e593fd8fa55c8a5f05dc3913bf7d8b9deabf
klcysn <klcysn@users.noreply.github.com>
2021-02-21 23:57:52 +0100

    Complete js file

--- api_request/index.js
+++ api_request/index.js
@@ -1,9 +1,16 @@
 function fetchData(){
     fetch("https://api.kanye.rest").then((response)=>response.json()).then(function(data){
         document.getElementById("result").innerHTML = data.quote
-        document.getElementById("result").style.transform = rotate("360deg")
     })
 }
 
+function onClick(){
+    document.getElementById("result").style.backgroundColor = `#b6${Math.floor(Math.random() * 10000)}`
+    document.getElementById("button").style.backgroundColor = `#b6${Math.floor(Math.random() * 10000)}`
+    fetchData()
+    document.getElementById("result").style.transform = document.getElementById("result").style.transform == "rotateY(360deg)" ? "rotateY(-360deg)" : "rotateY(360deg)"
+}
 
-document.querySelector("#button").addEventListener("click", fetchData)
+
+document.querySelector("#button").addEventListener("click", onClick)
+document.querySelector("#mover").addEventListener("click", onClick)
