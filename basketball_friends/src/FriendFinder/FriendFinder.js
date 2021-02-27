6e2766c239dcb4a7216f71cd48fd61f2a38ac3f0
klcysn <klcysn@users.noreply.github.com>
2021-02-27 12:54:16 +0100

    Add find friend and add friend functions

--- basketball_friends/src/FriendFinder/FriendFinder.js
+++ basketball_friends/src/FriendFinder/FriendFinder.js
@@ -4,19 +4,36 @@ import logo from "../assets/logo.png"
 
 
 export default class FriendFinder extends React.Component{
+
+    state={
+        data : {},
+        friends : []
+    }
+
+    fetchPeople = () =>{
+        fetch("https://randomuser.me/api/").then((friend)=>friend.json()).then((friend)=>this.setState({data: friend.results[0]}))
+    }
+
+    addFriend = () =>{
+        this.setState({friends: [...this.state.friends, this.state]})
+    }
+
+    
     render(){
+        console.log(this.state.friends)
         const {name, lastName} = this.props
         return(
             
         <div className="friend-finder">
-            <img className="friend-logo" src={logo} alt="human" />
+            <img className="friend-logo" src={this.state.data.picture?.large} alt={this.state.data.name?.first} />
             <div>
             Kişi bilgileri
             </div>
-            <h2>{name} {lastName}</h2>
+            <h2>{this.state.data.name?.first} {this.state.data.name?.last}</h2>
+            <h3>Nationality: {this.state.data?.nat}</h3>
             <div className="button-container">
-                <button>Find a Friend</button>
-                <button>Add a Friend</button>
+                <button onClick={this.fetchPeople}>Find a Friend</button>
+                <button onClick={this.addFriend}>Add As a Friend</button>
             </div>
         </div>
         )
